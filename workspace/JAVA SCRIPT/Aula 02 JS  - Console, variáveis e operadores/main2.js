089ead1540536d189a0f78536dc0bc9f76c833d0
herculeslbs <herculeslbs@gmail.com>
2022-04-27 21:26:25 -0300

    subindo aula e exercicio 02 js

---
+++ workspace/JAVA SCRIPT/Aula 02 JS  - Console, variáveis e operadores/main2.js
@@ -0,0 +1,6 @@
+const pessoa = 'Hercules';
+const altura = 1.73;
+const peso = 75;
+const IMC = (peso/(altura*altura))
+//console.log(peso/(altura*altura))
+console.log(`${pessoa} possui a altura: ${altura} e peso: ${peso}, seu IMC:${IMC}.`)
