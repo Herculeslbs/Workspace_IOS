09de534049de21f42c6f8aadaf86b30a532c2501
herculeslbs <herculeslbs@gmail.com>
2022-05-02 14:00:38 -0300

    subindo exercicio logica

---
+++ workspace/JAVA SCRIPT/Aula 03 JS - Funções/Exercício logica - pt1/media da turma.js
@@ -0,0 +1,7 @@
+let mediaAna = 8
+let mediaJoao = 9
+let mediaMaria = 7
+let mediaPedro = 6
+
+let mediaTurma = (mediaAna + mediaJoao + mediaMaria + mediaPedro) / 4
+console.log(mediaTurma)
---
+++ workspace/JAVA SCRIPT/Aula 03 JS - Funções/Exercício logica - pt1/reajuste salarial.js
@@ -0,0 +1,7 @@
+let salarioAtual = 2000  
+let salarioPorcentagem = 13 / 100
+let salarioAjustado = salarioAtual * salarioPorcentagem + salarioAtual
+
+console.log(`Seu salario atual é: R$${salarioAtual} reais`)
+console.log(`O valor de reajuste será de: R$${salarioAtual * salarioPorcentagem} reais`)
+console.log(`O valor total com reajuste será de: R$${salarioAjustado} reais`)
