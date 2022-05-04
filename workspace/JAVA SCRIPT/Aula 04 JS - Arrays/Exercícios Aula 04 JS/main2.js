c077e45d6decf351b2c1be3633c41d0b118435ae
herculeslbs <herculeslbs@gmail.com>
2022-05-04 01:16:20 -0300

    subindo exercícios aula 04 JS

---
+++ workspace/JAVA SCRIPT/Aula 04 JS - Arrays/Exercícios Aula 04 JS/main2.js
@@ -0,0 +1,24 @@
+// template string
+console.log(`**Arrays unidimensionais**
+`);
+// template string
+console.log(`Lista de alunos.
+`);
+//Arrays unidimensionais
+const lista = ['Joaquim', 'José', 'Silva', 'Xavier', 'Ana'];
+console.log(lista);
+// template string
+console.log(`**Arrays bidimensionais**
+`);
+
+// template string
+console.log(`Lista de alunos, notas, ano de nascimento e turma.
+`);
+//Arrays bidimensionais
+const lista1 = [
+    ['Joaquim', 'José', 'Silva', 'Xavier', 'Ana'],
+    [10, 9, 8, 7, 6],
+    [1996, 1997, 1998, 1999, 2000],
+    ['1°C', '2°A', '3°D','1°A','3°B'],
+];
+console.log(lista1);
