01c6ff9d560e29da03a339a83884d3b979a94d4a
herculeslbs <herculeslbs@gmail.com>
2022-05-11 21:51:23 -0300

    subindo aula 07 JS

---
+++ workspace/javaScript/Aula_07_JS_estruturasCondicionais/exercicioAula_07_JS/main.js
@@ -0,0 +1,29 @@
+// Array de objetos
+const colaboradores = [
+    {
+        Nome: 'Maria',
+        Salário: 1000, //7,5%
+        CLT: 'sim',
+        PJ: 'não',
+    },
+    {
+        Nome: 'João',
+        Salário: 2000, //9%
+        CLT: 'não',
+        PJ: 'sim',
+    },
+    {
+        Nome: 'Pedro',
+        Salário: 3000, //12%
+        CLT: 'sim',
+        PJ: 'não',
+    },
+    {
+        Nome: 'Aline',
+        Salário: 4000, //14%
+        CLT: 'não',
+        PJ: 'sim',
+    },
+
+];
+
---
+++ workspace/javaScript/Aula_07_JS_estruturasCondicionais/main.js
@@ -0,0 +1,82 @@
+// Estrutura condicional simples
+// Igual duplo
+const a = 10;
+if (a == 10) {
+console.log('a é 10');
+}
+const b = '10';
+if (b == 10) {
+console.log('b é 10');
+}
+
+// Triplo igual - verifica o tipo
+const c = 10;
+if (c === 10) console.log('c é 10');
+const d = '10';
+if (d === 10) {
+console.log('d é 10');
+}
+console.log('Estou fora do if que compara string');
+
+// Estrutura condicional composta
+console.clear();
+const e = '10';
+if (e === 10) console.log('e é o numeral 10');
+else {
+console.log(`Resultado da condição: ${e === 10}`);
+console.log('e não é o numeral 10');
+}
+
+// Multiplas condições
+const f = 4;
+const g = 11;
+if (f > 5 || g > 10) {
+console.log(`Condição é ${f > 5 || g > 10}`);
+console.log('f é maior do que 5 ou g é maior do 10');
+} else console.log('f não é maior do que 5 e g não é maior do 10');
+if (f > 5 && g > 10) console.log('f é maior do que 5 e g é maior do 10');
+else {
+console.log(`Condição é ${f > 5 && g > 10}`);
+console.log('f não é maior do que 5 ou g não é maior do 10');
+}
+
+// Desvios encadeados
+const num = 'Ola';
+if (num % 2 == 0 && !isNaN(num)) console.log('Número par!');
+else if (num % 2 != 0 && !isNaN(num)) console.log('Número ímpar');
+else console.log('Não é um número!');
+
+// Desvios encadeados
+const num1 = 10;
+if (num1 % 2 == 0 && !isNaN(num1)) console.log('Número par!');
+else if (num1 % 2 != 0 && !isNaN(num1)) console.log('Número ímpar');
+else console.log('Não é um número!');
+
+// Operador ternário - exemplo 1
+let resultado = 3 > 4 ? "Sim" : "Não";
+console.log(resultado)
+
+// Operador ternário - exemplo 2
+let result = Math.PI < 4 ? console.log('Sim') : console.log('Não');
+
+Math.PI; // retorna 3.141592653589793
+Math.E // retorna o número de Euler
+Math.SQRT2 // retorna a raiz quadrada de 2
+Math.SQRT1_2 // retorna a raiz quadrada de 1/2
+Math.LN2 // retorna o logaritmo natural de 2
+Math.LN10 // retorna o logaritmo natural de 10
+
+// Switch
+const h = 11;
+const cor = h > 10 ? 'vermelha' : 'azul';
+switch (cor) {
+case 'vermelha':
+console.log('A cor é vermelha');
+break;
+case 'azul':
+console.log('A cor é azul');
+break;
+default:
+console.log('A cor não é vermelha ou azul');
+break;
+}
