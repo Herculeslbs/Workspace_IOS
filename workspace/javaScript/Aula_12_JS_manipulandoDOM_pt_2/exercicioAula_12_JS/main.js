57bcb94fe091377e46404ee52f5212fc34502470
herculeslbs <herculeslbs@gmail.com>
2022-05-24 21:50:39 -0300

    subindo aula 14 js

--- workspace/javaScript/Aula_12_JS_manipulandoDOM_pt_2/exercicioAula_12_JS/main.js
+++ workspace/javaScript/Aula_12_JS_manipulandoDOM_pt_2/exercicioAula_12_JS/main.js
@@ -1,3 +1,6 @@
-alert(`Conferir Console`);
+//alert(`Conferir Console`);
 
 let btn = document.getElementsByClassName('B_12');
+//btn.style.border = 'solid 2px #D9BC66'
+btn.style.padding = '5.rem'
+btn.style.margin = '7.rem'
---
+++ workspace/javaScript/Aula_14_JS_manipulandoDOM_pt_4/exercicioAula_14_JS/js/main.js
@@ -0,0 +1,6 @@
+// Método querySelector
+const btn = document.querySelector('.btn');
+btn.style.background = 'green';
+const btn1 = document.querySelector('.btn1');
+btn1.style.background = 'red';
+
