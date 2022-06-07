9e11d8abba2af5629f52330976601d4a6b99f7b3
herculeslbs <herculeslbs@gmail.com>
2022-06-06 21:52:58 -0300

    subindo aula 05 React

---
+++ workspace/React/Aula_05_agenda_de_compromissos/tema_05_agenda/src/App.js
@@ -0,0 +1,11 @@
+import Header from "./Componentes/Header";
+
+function App() {
+    return (
+        <div className="container">
+            <Header title = "tarefas"/>
+        </div>
+    );
+}
+
+export default App;
