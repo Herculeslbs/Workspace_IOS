bdced9ea36f1e6ed116b0d641b0279f616d42930
herculeslbs <herculeslbs@gmail.com>
2022-07-03 22:46:46 -0300

    atualizando

--- workspace/Java/Aula_09_Strings/Exercicio_Aula_09_Java/src/ArrayString.java
+++ workspace/Java/Aula_09_Strings/Exercicio_Aula_09_Java/src/ArrayString.java
@@ -5,14 +5,56 @@
 - Comparar a string igualando caracteres (ToUpperCase)
 - Comparar a string eliminando caracter em branco (trim)
 - Após receber as strings sobrepor o conteúdo (replace)
-em caixa baixa (toLowerCase)
+  em caixa baixa (toLowerCase)
 - Exibir a listagem de frutas recebidas*/
+import java.util.Scanner;
 
 public class ArrayString {
-
 	public static void main(String[] args) {
 		
-
+		 Scanner entrada = new Scanner (System.in);
+	     String frutas[] = new String[5];
+	     
+	     int i;
+	     
+	     for(i = 0; i < frutas.length; i++ ) {
+	      System.out.println("Digite o nome da " + (i+1) + "º frutas:");
+	      frutas[i] = entrada.nextLine();	          
+	    
+	     var vazio = frutas[i].isEmpty();
+	     var tamanho = frutas[i].length();
+	     
+	   	     
+	     if(vazio == true) {
+	         System.out.println("Este indice possui espaço vazio.");
+	     }
+	     else {
+	         System.out.println("Este indice não possui espaço vazio.");
+	     }
+	     if(tamanho >= 3) {
+	    	 System.out.println("Este indice possui " + frutas[i].length() + " caracteres."); 
+	     }
+	     else {
+	    	 System.out.println("Este indice possui menos de 3 caracteres.");
+	     }
+	         System.out.println();	         
+	     } 
+	  		
+	    for(i = 0; i < frutas.length; i++ ) {
+	    	System.out.print("[" + frutas[i].toUpperCase().trim() + "] ");	    	          	    
+	    }
+	    
+	    System.out.println();
+	    
+	    String frutas1[] = new String[frutas.length];
+	    
+	    for(i = 0; i < frutas.length; i++ ) {
+	    	frutas1[i] = frutas[i];
+	    }
+	    	System.out.println();
+	    for(i = 0; i < frutas.length; i++ ) {
+	    	System.out.print("[" + frutas1[i].replace(frutas1[i], frutas1[i].toLowerCase()) + "] ");
+	    }
+	    	   
 	}
-
 }
